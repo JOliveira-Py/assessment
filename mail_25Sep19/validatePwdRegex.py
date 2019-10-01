df2830e9270e805eb7fcc5ff4cf162d606215aa1
João Oliveira <JOliveira-Py@users.noreply.github.com>
2019-10-01 19:01:57 +0100

    small corrections and improvements

--- mail_25Sep19/validatePwdRegex.py
+++ mail_25Sep19/validatePwdRegex.py
@@ -4,14 +4,14 @@ def validatePwd(pwds):
     pwdList = pwds.split(',')
     validated = []
     for i in pwdList:
-        if len(i) >= 6 and len(i) <=12\
-           and re.search('[a-z]',i)\
-           and re.search('[A-Z]',i)\
-           and re.search('[0-9]',i)\
-           and re.search('[$#@]',i):
+        if (
+            len(i) >= 6 and len(i) <=12
+            and re.search('[a-z]',i)
+            and re.search('[A-Z]',i)
+            and re.search('[0-9]',i)
+            and re.search('[$#@]',i)
+        ):
             validated = validated + [i]
-        else:
-            print("else")
     return validated
 
 pwdStr = input("Please insert list of passwords (separated by comma)\n")
