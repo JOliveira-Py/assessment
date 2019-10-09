c09cdfece8c933627bbc7437103a854599ff6f3e
João Oliveira <JOliveira-Py@users.noreply.github.com>
2019-10-09 03:02:20 +0100

    Minor changes (prevented showing same machine in more than one group)

--- mail_03Oct19/jsonImportMatchV3.py
+++ mail_03Oct19/jsonImportMatchV3.py
@@ -8,36 +8,47 @@ def matchMachines(userFile='data.txt'):
 
     for machine in range(len(data)):
         add = True
-        # Check if machine was already matched
+        # Check if 'machine' was already matched
         for j in groups:
             if data[machine]['name'] in j:
                 add = False
                 break
         if add:        
-            # Store info about 'original' machine
+            # Store info about original 'machine'
             color = data[machine]['color']
             minV = data[machine]['min_version']
             maxV = data[machine]['max_version']
             # Matching machines will be stored here
             newGroup = data[machine]['name']
             for comparison in range(machine+1,len(data)):
-                intersects = (
-                    (minV <= data[comparison]['min_version'] <= maxV)
-                    or (minV <= data[comparison]['max_version'] <= maxV))
-                if data[comparison]['color'] == color and intersects:
-                    checkAllGroup = True
-                    # Check if machine in comparison is version compatible with all machines previsously grouped
-                    for k in data:
-                        if k['name'] in newGroup:
-                            intersectsAlso = (
-                                (data[comparison]['min_version'] <= k['min_version'] <= data[comparison]['max_version'])
-                                or (data[comparison]['min_version'] <= k['max_version'] <= data[comparison]['max_version']))
-                            if not intersectsAlso:
-                                checkAllGroup = False
-                    # Add new machine to matching machines
-                    if checkAllGroup:
-                        newGroup += ' ' + data[comparison]['name']
-            # Store new group of matching machines
+                added = False
+                for k in groups:
+                    if data[comparison]['name'] in k:
+                        added = True
+                        break
+                if not added:
+                    intersects = (
+                        (minV <= data[comparison]['min_version'] <= maxV)
+                        or (minV <= data[comparison]['max_version'] <= maxV))
+                    if data[comparison]['color'] == color and intersects:
+                        checkAllGroup = True
+                        # Store info about 'comparison' machine
+                        minV2 = data[comparison]['min_version']
+                        maxV2 = data[comparison]['max_version']
+                        # Check if 'comparison' machine is version compatible with all
+                        # machines previously added to current 'newGroup'
+                        for m in data:
+                            if m['name'] in newGroup:
+                                intersectsAlso = (
+                                    (minV2 <= m['min_version'] <= maxV2)
+                                    or (minV2 <= m['max_version'] <= maxV2))
+                                if not intersectsAlso:
+                                    checkAllGroup = False
+                                    break
+                        # Add 'comparison' machine to matching machines 'newGroup'
+                        if checkAllGroup:
+                            newGroup += ' ' + data[comparison]['name']
+            # Store 'newGroup' of matching machines
             groups.append(newGroup)
     return groups
 
